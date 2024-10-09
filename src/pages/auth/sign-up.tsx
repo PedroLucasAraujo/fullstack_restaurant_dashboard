6981332190fabd61de7479d8985c42abe3a8d354
PedroLucasAraujo <pedrolpriv@gmail.com>
2024-10-09 19:17:43 -0300

    feat: create restaurant register integration with backedn

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -6,6 +6,8 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { toast } from "sonner";
 import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { registerRestaurant } from "@/api/register-restaurant";
 
 const signUpForm = z.object({
   restaurantName: z.string(),
@@ -23,17 +25,25 @@ export function SignUp() {
     handleSubmit,
     formState: { isSubmitting },
   } = useForm<SignUpForm>();
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant,
+  });
 
   async function handleSignUp(data: SignUpForm) {
     try {
-      console.log(data);
+      await registerRestaurantFn({
+        email: data.email,
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        phone: data.phone,
+      });
 
       await new Promise((r) => setTimeout(r, 2000));
 
       toast.success("Estabelecimento cadastrado com sucesso!", {
         action: {
           label: "Login",
-          onClick: () => navigate("/sign-in"),
+          onClick: () => navigate(`/sign-in?email=${data.email}`),
         },
       });
     } catch {
