802e69020a0c60a2a1ab01d4a4df163f8063683f
PedroLucasAraujo <pedrolpriv@gmail.com>
2024-10-10 10:09:43 -0300

    feat: integrate orders pagination

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,3 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import { useSearchParams } from "react-router-dom";
+import { z } from "zod";
+
+import { getOrders } from "@/api/get-orders";
+import { Pagination } from "@/components/pagination";
 import {
   Table,
   TableBody,
@@ -5,25 +12,37 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Helmet } from "react-helmet-async";
-import { OrderTableRow } from "./order-table-row";
+
 import { OrderTableFilters } from "./order-table-filters";
-import { Pagination } from "@/components/pagination";
-import { useQuery } from "@tanstack/react-query";
-import { getOrders } from "@/api/get-orders";
+import { OrderTableRow } from "./order-table-row";
 
 export function Orders() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const pageIndex = z.coerce
+    .number()
+    .transform((page) => page - 1)
+    .parse(searchParams.get("page") ?? "1");
+
   const { data: result } = useQuery({
-    queryKey: ["orders"],
-    queryFn: getOrders,
+    queryKey: ["orders", pageIndex],
+    queryFn: () => getOrders({ pageIndex }),
   });
+
+  function handlePaginate(pageIndex: number) {
+    setSearchParams((state) => {
+      state.set("page", (pageIndex + 1).toString());
+
+      return state;
+    });
+  }
 
   return (
     <>
       <Helmet title="Pedidos" />
+
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-
         <div className="space-y-2.5">
           <OrderTableFilters />
 
@@ -33,10 +52,10 @@ export function Orders() {
                 <TableRow>
                   <TableHead className="w-[64px]"></TableHead>
                   <TableHead className="w-[140px]">Identificador</TableHead>
-                  <TableHead className="w-[180px]">Realizado Há</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
                   <TableHead className="w-[140px]">Status</TableHead>
                   <TableHead>Cliente</TableHead>
-                  <TableHead className="w-[140px]">Total do Pedido</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
                   <TableHead className="w-[164px]"></TableHead>
                   <TableHead className="w-[132px]"></TableHead>
                 </TableRow>
@@ -50,7 +69,14 @@ export function Orders() {
             </Table>
           </div>
 
-          <Pagination pageIndex={0} totalCount={105} perPage={10} />
+          {result && (
+            <Pagination
+              onPageChange={handlePaginate}
+              pageIndex={result.meta.pageIndex}
+              totalCount={result.meta.totalCount}
+              perPage={result.meta.perPage}
+            />
+          )}
         </div>
       </div>
     </>
